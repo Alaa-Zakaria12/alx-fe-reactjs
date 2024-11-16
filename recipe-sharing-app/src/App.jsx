13c4d18e8767f9b9c8ece8fd0969e06ba1a002be
Alaa Zakaria <ana.alaa128@gmail.com>
2024-11-16 13:19:48 +0200

    modifing this file

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,20 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 import EditRecipeForm from './components/EditRecipeForm';
-import { useRecipeStore } from './stores/recipeStore';
-import SearchBar from './components/SearchBar';
+import AddRecipeForm from './components/AddRecipeForm';
 
 const App = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
-  
   return (
     <Router>
       <div>
-        <SearchBar />
+        <h1>Recipe Sharing App</h1>
         <Routes>
-          <Route path="/" element={<RecipeList recipes={recipes} />} />
+          <Route path="/" element={<RecipeList />} />
           <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
           <Route path="/edit/:recipeId" element={<EditRecipeForm />} />
+          <Route path="/add" element={<AddRecipeForm />} />
         </Routes>
       </div>
     </Router>
